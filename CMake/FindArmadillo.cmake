2b600bb9e6c5be0b77bb0c76dfa82d2f66e30689
RouckWind <infinite11145@gmail.com>
2022-04-14 20:02:27 +0200

    test

--- CMake/FindArmadillo.cmake
+++ CMake/FindArmadillo.cmake
@@ -4,7 +4,7 @@
 #[=======================================================================[.rst:
 FindArmadillo
 -------------
-//sdsds dfsds dsd
+//sdsds dfsds dsd dsds
 Find the Armadillo C++ library.
 Armadillo is a library for linear algebra & scientific computing.
 
